e842289ecc197fdf80486a11f9dd2fe13839dec2
poonamk <poonamk@thoughtwin.com>
2023-02-09 11:30:25 +0530

    *

--- djangogit/settings.py
+++ djangogit/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-9pg07%^r$!tq4cb_-$uiw!3-+xm4db!f10e&um8tlxmrqc81et
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
