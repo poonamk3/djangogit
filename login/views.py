61e9104d55cb64bbad004fa175506d3ca3e0c251
poonamk <poonamk@thoughtwin.com>
2023-02-09 12:07:13 +0530

    add new app

---
+++ login/views.py
@@ -0,0 +1,5 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+def methodinfo(request):  
+    return HttpResponse("Http request is: "+request.method+)  
