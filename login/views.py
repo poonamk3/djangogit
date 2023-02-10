be54dc2ea66c05634041a0e31948d3ec175becf4
poonamk <poonamk@thoughtwin.com>
2023-02-10 12:43:44 +0530

    change views

--- login/views.py
+++ login/views.py
@@ -3,4 +3,4 @@ from django.http import HttpResponse
 # Create your views here.
 
 def methodinfo(request):  
-    return HttpResponse("Hii request is:"+request.method)  
+    return HttpResponse("my request is:"+request.method)  
